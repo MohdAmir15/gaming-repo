e113e03908edb3f0e148775f0c254459579f0958
MohdAmir <mohdamir1315@gmail.com>
2024-04-03 10:17:09 +0800

    Adding projectiles

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ BG=pygame.transform.scale(pygame.image.load("bg.jpg"),(WIDTH,HEIGHT))
 PLAYER_WIDTH=40
 PLAYER_HEIGHT=60
 PLAYER_VEL=5
+STAR_WIDTH=10
+STAR_HEIGHT = 20
 FONT=pygame.font.SysFont("comicscans",30)
 
 def draw(player,elapsed_time):
@@ -32,9 +34,24 @@ def main():
     start_time=time.time()
     elapsed_time=0
 
+    star_add_increment=2000
+    star_count=0
+
+    stars=[]
+
     while run:
-        clock.tick(60)
+        star_count==clock.tick(60)
         elapsed_time=time.time()-start_time
+
+        if star_count > star_add_increment:
+            for _ in range(3):
+                star_x = random.randint(0,WIDTH - STAR_WIDTH)
+                star = pygame.Rect(star_x, STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
+                stars.append(star)
+
+            star_add_increment = max(200,star_add_increment -50 )
+            star_count = 0
+
 
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
